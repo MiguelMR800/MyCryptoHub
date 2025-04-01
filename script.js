0838a16ef26c816792407fa5a882347ee0d2d2dd
Miguel <159823915+MiguelMR800@users.noreply.github.com>
2025-04-01 05:07:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
   const body = document.body;
-
   const sections = document.querySelectorAll('section');
+
   sections.forEach(section => {
     const rect = section.getBoundingClientRect();
     const id = section.id;
 
     if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-      body.className = '';
-      body.classList.add(`${id}-bg`);
+      body.className = ''; // Poista vanhat luokat
+      body.classList.add(`${id}-bg`); // Lisää uuden taustaluokan
     }
   });
+});
+
+window.addEventListener('load', () => {
+  const firstSection = document.querySelector('section');
+  if (firstSection) {
+    document.body.classList.add(`${firstSection.id}-bg`);
+  }
 });
