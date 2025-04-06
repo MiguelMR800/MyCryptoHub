e6ba483a324322569423c8de3cea8b80558f5fd8
Miguel <159823915+MiguelMR800@users.noreply.github.com>
2025-04-06 08:44:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function copy(id) {
-  const text = document.getElementById(id).innerText;
-  navigator.clipboard.writeText(text);
-  alert("Copied: " + text);
-}
+console.log('Chatbot & data live');
